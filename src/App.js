d9722073f74a0b93eff29b8826adaafa4142c729
Penina Wanjiru <penina.wanjiru@andela.com>
2020-01-09 17:49:17 +0300

    Add monster search functionality

--- src/App.js
+++ src/App.js
@@ -21,10 +21,12 @@ componentDidMount(){
 }
 
   render() {
+		const {monsters, searchField} = this.state;
+		const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return(
       <div className="App">
-				<input type='search' placeholder='search monsters' onChange={e=> this.setState({searchField:e.target.value}, () => console.log(this.state))}/>
-				<CardList monsters={this.state.monsters}/>
+				<input type='search' placeholder='search monsters' onChange={e=> this.setState({searchField:e.target.value})}/>
+				<CardList monsters={filteredMonsters}/>
       </div>
     );
   }
