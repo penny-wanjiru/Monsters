72f0c6e75dfb10d3f20f1ff55e3637fb0b4dc53e
Penina Wanjiru <penina.wanjiru@andela.com>
2020-01-09 17:06:11 +0300

    Set search state

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ componentDidMount(){
   render() {
     return(
       <div className="App">
-				<input type='search' placeholder='search monsters' onChange={e=> {console.log(e.target.value);}}/>
+				<input type='search' placeholder='search monsters' onChange={e=> this.setState({searchField:e.target.value}, () => console.log(this.state))}/>
 				<CardList monsters={this.state.monsters}/>
       </div>
     );
